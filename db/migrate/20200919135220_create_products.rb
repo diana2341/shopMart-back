35657203406a5e9a720cfbecfbfde418dc8f41d3
diana2341 <dianaponce2341@gmail.com>
2020-09-19 09:58:46 -0400

    created products and carts table

---
+++ db/migrate/20200919135220_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.integer :products_id
+      t.float :price
+      t.integer :amount
+      t.string :color 
+      t.string :size
+      t.string :images 
+      t.string :description
+      t.string :categories
+
+      t.timestamps
+    end
+  end
+end
